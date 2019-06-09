a562e873e2721b615a5e8717c87331f54fc4d105
hradec <me@hradec.com>
2019-06-09 02:53:49 -0700

    gathering the state of jobs and updating cards automatically.

--- updateBackupBoard.py
+++ updateBackupBoard.py
@@ -12,8 +12,10 @@ api = WekanApi("http://192.168.0.16:8080/wekan", eval(''.join(open("userpasswd.t
 
 
 jobs=[]
-d={}
+d = {}
+cards = {}
 for b  in api.get_user_boards('BACKUP'):
+	print b.get_custom_fields()
 	d[b.title] = {}
 	d[b.title][".class"] = b
 	for list in b.get_cardslists():
@@ -22,26 +24,30 @@ for b  in api.get_user_boards('BACKUP'):
 		for card in list.get_cards():
 			d[b.title][list.title][card.title]=card
 			jobs.append(card.title)
+			cards[ card.title.split('\n')[0] ] = card
 
-
-pp(d)
 
 paths=[]
 j=''.join(jobs)
 for p in glob("/atomo/jobs/*"):
 	jobNumber = int(os.path.basename(p).split('.')[0])
 	if jobNumber < 9000 and jobNumber > 0:
-		W = os.path.basename(p) in j
-		if not W:
-			paths += [p]
-			print W,p
+		hasCard = os.path.basename(p) in j
+		# if a card exists
+		if hasCard:
+			size=''.join(os.popen("du -shc %s | grep total" % p).readlines()).split()[0].strip()
+			disco=os.path.dirname(p)
+			mover=""
+			posicao=""
+			title=os.path.basename(p)
+			title+="\ndisco: %s" % disco
+			title+="\nmover: %s" % mover
+			title+="\nposicao: %s" % posicao
+			title+="\ntamanho: %s" % size
+			cards[ os.path.basename(p) ].modify( title=title )
 
-paths.sort()
-pp(paths)
-
-
-
-for b  in api.get_user_boards('BACKUP'):
-	for l in b.get_cardslists('JOBS'):
-		for p in paths:
-			l.add_card(p)
+		# or else create a new card
+		# else:
+		# 	for b  in api.get_user_boards('BACKUP'):
+		# 		for l in b.get_cardslists('JOBS'):
+		# 			l.add_card(os.path.basename(p))
