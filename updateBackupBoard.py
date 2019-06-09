6b33dd54a9c54f88ee98d518dbe7f53b40de6345
hradec <me@hradec.com>
2019-06-09 03:25:57 -0700

    updates the cards with the real path and size of jobs

--- updateBackupBoard.py
+++ updateBackupBoard.py
@@ -30,11 +30,16 @@ for b  in api.get_user_boards('BACKUP'):
 paths=[]
 j=''.join(jobs)
 for p in glob("/atomo/jobs/*"):
+	p = os.path.abspath(p)
+	if os.path.islink(p):
+		p = os.readlink(p)
+	p = p.rstrip('/')
 	jobNumber = int(os.path.basename(p).split('.')[0])
 	if jobNumber < 9000 and jobNumber > 0:
 		hasCard = os.path.basename(p) in j
 		# if a card exists
 		if hasCard:
+			print p
 			size=''.join(os.popen("du -shc %s | grep total" % p).readlines()).split()[0].strip()
 			disco=os.path.dirname(p)
 			mover=""
