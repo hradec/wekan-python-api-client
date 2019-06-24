888237e29764945e8b7a30d0c6c027d4afc4f767
hradec <me@hradec.com>
2019-06-23 17:02:09 -0700

    including LTO folders in the card generation/updating.  Try to maintain last-modification from the card, when updating a card in the LTO. Added some TODO: to start writing automation (rsync/delete). Started code to create a "Total" card for each list.

--- updateBackupBoard.py
+++ updateBackupBoard.py
@@ -45,7 +45,7 @@ if len(ltoBackup) > 3:
 print ltoBackup
 ltoFreeSpace = ''.join(os.popen("ssh root@nexenta.local 'df -h | grep LTO'").readlines()).strip().split()[-3]
 print ltoFreeSpace
-ltoLS = [ x.strip() for x in os.popen("ssh root@nexenta.local 'ls -1 /LTO/'").readlines() ]
+ltoLS = [ '/LTO/%s' % x.strip() for x in os.popen("ssh root@nexenta.local 'ls -1 /LTO/'").readlines() ]
 print ltoLS
 
 # loop over jobs and update weekan cards with size and other info
@@ -55,15 +55,15 @@ folders += glob("/.MOOSEFS/atomo/jobs/*")
 folders += glob("/smb/Backups-3ds/atomo/jobs/*")
 folders.sort()
 repetidos={}
-for p in folders:
+for p in folders+ltoLS:
 	p = os.path.abspath(p).replace('//','/').rstrip('/')
 	if os.path.islink(p):
 		p = os.readlink(p)
 	p = p.replace('//','/').rstrip('/')
 
-	if not os.path.exists(p):
+	if not os.path.exists(p) and not '/LTO' in p:
 		toRemove.append(p)
-	elif os.path.isdir(p) and not os.path.islink(p):
+	elif ( os.path.isdir(p) and not os.path.islink(p) ) or '/LTO' in p:
 		if os.path.basename(p) not in repetidos:
 			repetidos[ os.path.basename(p) ] = [p]
 		else:
@@ -84,7 +84,10 @@ gifs={
 gif_counter={
 	'esperando' : 0,
 }
-for job in repetidos.keys():
+repetidos_sorted = repetidos.keys()
+repetidos_sorted.sort()
+lto_total = 0
+for job in repetidos_sorted:
 	# print job
 	repetidos[job].sort()
 	p = [ x for x in repetidos[job] if '/atomo' in x[0:7] ]
@@ -96,18 +99,56 @@ for job in repetidos.keys():
 	jobNumber = int(os.path.basename(p).split('.')[0])
 
 	# only consider directories
-	if jobNumber < 9000 and jobNumber > 0: # and jobNumber==587:
+	if jobNumber < 9000 and jobNumber > 0:
+	  # if jobNumber==621:
+
+		print p
+		# get the title of the card, if its already exist
+		card_title = ""
+		hasCard = os.path.basename(p) in j and os.path.basename(p) in cards.keys()
+		if hasCard:
+			card_title = cards[ os.path.basename(p) ].data['title'].strip()
 
 		# calculate size of jobs just once a day!
-		print p
 		l='/tmp/%s.disk_usage.log' % p.strip('/').replace('/','_')
-		lm='/tmp/%s.last_modified.log' % p.strip('/').replace('/','_')
 		if not os.path.exists(l) or ( ( time.time() - int(os.stat(l)[-1]) ) /60 /60 ) > 24  or  os.stat( l )[6] == 0:
-			os.popen( 'du -shc %s 2>/dev/null | grep total > %s ' % ( p, l ) )
+			if '/LTO' in p:
+				tamanho = 0
+				# if we have tamanho in the title
+				# if 'tamanho' in card_title:
+				# 	# extract the value from it and save to the log file.
+				# 	m = [ x for x in card_title.split('\n') if 'tamanho' in x ]
+				# 	if m:
+				# 		if '**' in m[0]:
+				# 			m = m[0].split('**')[1]
+				# 		else:
+				# 			m = m[0].split(' ')[1]
+				#
+				# 		mv = float(''.join([x for x in m if x.isdigit() or x=='.']))
+				# 		if mv > 0:
+				# 			if 'G' in m.upper():
+				# 				mv = mv/1000
+				# 			elif 'T' in m.upper():
+				# 				mv = mv
+				# 			else:
+				# 				mv = mv/1000/1000
+				# 		tamanho = mv
+				if not tamanho:
+					os.popen( '''ssh root@192.168.0.16 "du -shc %s 2>/dev/null" | grep total > %s ''' % ( p, l ) )
+				else:
+					os.popen( '''echo "%.2fT" > %s ''' % ( tamanho, l ) )
+			else:
+				os.popen( 'du -shc %s 2>/dev/null | grep total > %s ' % ( p, l ) )
 
+		# get the last modification date
+		lm='/tmp/%s.last_modified.log' % p.strip('/').replace('/','_')
 		if not os.path.exists(lm) or ( ( time.time() - int(os.stat(lm)[-1]) ) /60 /60 ) > 24  or  os.stat( lm )[6] == 0:
-			# os.popen( "sudo find %s -type f -printf '%%T@ %%p\n' | sort -n | tail -1 | date -d@$(awk '{print $1}') 2>/dev/null  >  %s" % ( p, lm ) )
-			os.popen( "sudo find %s -type f -printf '%%T@ %%p\n' | sort -n | tail -1 2>/dev/null  >  %s " % ( p, lm ) )
+			if '/LTO' in p:
+				if 'modificado' not in card_title:
+					# calculate from the TAPE folder
+					os.popen( '''ssh root@192.168.0.16 "find %s -type f -printf '%%T@ %%p\n'" | sort -n | tail -1 2>/dev/null  >  %s ''' % ( p, lm ) )
+			else:
+				os.popen( "sudo find %s -type f -printf '%%T@ %%p\n' | sort -n | tail -1 2>/dev/null  >  %s " % ( p, lm ) )
 
 		size= ''.join(open( l ).readlines()).split()[0].strip()
 		disco=os.path.dirname(p)
@@ -115,8 +156,10 @@ for job in repetidos.keys():
 		posicao=""
 		last_modified=""
 		extra=""
-		months=0
 
+		# months is later to write the position string...
+		months=0
+		# write the last modified string
 		if os.path.exists( lm ):
 			epoch=''.join(open( lm ).readlines()).strip().split()
 			if len(epoch) > 1:
@@ -146,8 +189,15 @@ for job in repetidos.keys():
 				elif int(months) == 0:
 					meses=""
 				last_modified = '\nmodificado a:**<font color="%s"> %s%s</font>**' % ( color, meses, dias )
+		elif 'modificado' in card_title:
+			# keep last modification from card title
+			m = [ x for x in card_title.split('\n') if 'modificado' in x ]
+			print cards[ os.path.basename(p) ].data['title'].strip()
+			if m:
+				last_modified = '\n%s' % m[0]
 
-		hasCard = os.path.basename(p) in j and os.path.basename(p) in cards.keys()
+
+
 		if hasCard:
 			if cards[ os.path.basename(p) ].cardslist.title == "JOBS":
 				if months < 2:
@@ -172,20 +222,57 @@ for job in repetidos.keys():
 				if os.path.basename(p) in ltoBackup:
 					posicao="**movendo para o LTO...**"
 
+				m = [ x for x in card_title.split('\n') if 'tamanho' in x ]
+				if m:
+					if '**' in m[0]:
+						m = m[0].split('**')[1]
+					else:
+						m = m[0].split(' ')[1]
+					if 'G' in m.upper():
+						m = 1000/float(''.join([x for x in m if x.isdigit() or x=='.']))
+					elif 'T' in m.upper():
+						m = float(''.join([x for x in m if x.isdigit() or x=='.']))
+					else:
+						m = 1000/1000/float(''.join([x for x in m if x.isdigit() or x=='.']))
+
+					lto_total += m
+
 
 			elif "BKP" in cards[ os.path.basename(p) ].cardslist.title:
+				if 'esperando...' not in card_title:
+					# TODO: write the code to start rsync when cards have "esperando..." in it, and are in a BKP* list
+					print "\n\nNeed to write the code to start rsync when a %s is 'esperando...' in the LTO list\n\n" % job
+
 				posicao = "**esperando...**"
+				# if the path is being copied over right now... (ltoBackup tells us that!)
 				if os.path.basename(p) in ltoBackup:
 					posicao="**movendo para o LTO...**"
-				elif job in ltoLS:
-					rep = repetidos[job]+[ x for x in ltoLS if job in x ]
+
+				# if the job name is in the current loaded LTO...
+				elif job in [ os.path.basename(x) for x in ltoLS ]:
+					rep = repetidos[job]
+					# if we have more than one path for the job that is in
+					# the LTO tape, we need to delete the other ones
 					if len( rep )>1:
+						# TODO: we need to write the code to double check if the LTO copy is 100% the other paths.
+						# TODO: if we known the LTO copy is OK, then move it to a deleted folder to be deleted after 48hours
 						posicao = "**terminado - falta apagar %s**" % ', '.join(repetidos[job])
 					else:
+						# theres no other path for the JOB in the LTO
 						posicao = "**terminado - %s**" % cards[ os.path.basename(p) ].cardslist.title
 
+					# keep the size from the card title
+					# m = [ x for x in card_title.split('\n') if 'tamanho' in x ]
+					# if m:
+					# 	if '**' in m[0]:
+					# 		tsize = m[0].split('**')[1]
+					# 	else:
+					# 		tsize = m[0].split(' ')[1]
+					# 	if float(tsize) > 0:
+					# 		size = tsize
 
-		# set extra information
+
+		# set extra information (icons)
 		if 'esperando' in posicao:
 			# extra='<img src="%s" width=200 height=70>' % gifs['esperando'][random.randint(0, len(gifs['esperando'])-1)]
 			gif_counter['esperando'] += 1
@@ -270,6 +357,22 @@ for job in repetidos.keys():
 			else:
 				list.add_card( title )
 
+
+
+# lto_total
+# def createListTotal( p ):
+# 	list = cards[ os.path.basename(p) ].cardslist
+# 	b = list.board
+# 	title = "**%s - %%s total**" % ( list.title )
+# 	spaceCard = [ x for x in d[b.title][list.title].keys() if 'total**' in x.lower() ]
+# 	if spaceCard:
+# 		title = cards[ spaceCard[0].replace('*','') ].data['title']
+#
+# 		total = float(title.split(' ')[0])+
+# 		if title != cards[spaceCard[0].replace('*','')].data['title']:
+# 			cards[spaceCard[0].replace('*','')].modify( title=title )
+# 	else:
+# 		list.add_card( title )
 
 
 print "\nThe following jobs don't exist:"
