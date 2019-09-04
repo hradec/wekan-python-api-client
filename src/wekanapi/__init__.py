8d292eb1e31bef1abdaef959925c37bceacc8879
rhradec <me@hradec.com>
2019-09-04 01:50:14 -0700

    making session.put and session.post thread safe, since it seems to fail when running in a multithreaded script.

--- src/wekanapi/__init__.py
+++ src/wekanapi/__init__.py
@@ -1,6 +1,7 @@
 import requests
 from .models import Board
-
+import multiprocessing
+_LOCK = multiprocessing.Lock()
 
 class WekanApi:
     def api_call(self, url, data=None, authed=True, params=None):
@@ -15,12 +16,14 @@ class WekanApi:
             if params:
                 if data:
                     # print 1
+                    _LOCK.acquire( timeout=5000 )
                     api_response = self.session.put(
                         "{}{}".format(self.api_url, url),
                         data=data,
                         headers={"Authorization": "Bearer {}".format(self.token)},
                         proxies=self.proxies
                     )
+                    _LOCK.release()
                 else:
                     headers = {
                       'Content-Type': 'multipart/form-data',
@@ -28,21 +31,25 @@ class WekanApi:
                       'Authorization': "Bearer {}".format(self.token),
                     }
                     # print 2, headers, "{}{}".format(self.api_url, url)
+                    _LOCK.acquire( timeout=5000 )
                     api_response = self.session.put(
                         "{}{}".format(self.api_url, url),
                         params=params,
                         headers=headers if authed else {},
                         # proxies=self.proxies
                     )
+                    _LOCK.release()
             # add
             else:
                 # print 3
+                _LOCK.acquire( timeout=5000 )
                 api_response = self.session.post(
                     "{}{}".format(self.api_url, url),
                     data=data,
                     headers={"Authorization": "Bearer {}".format(self.token)} if authed else {},
                     proxies=self.proxies
                 )
+                _LOCK.release()
         return api_response.json()
 
     def __init__(self, api_url, credentials, proxies=None):
